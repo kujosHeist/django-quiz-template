e4b0e83b3e968c2175ee95f21ef8403f9ae4f34a
scart87 <shane.carty@hotmail.com>
2017-03-27 22:46:40 +0100

    added quiz, answer, question models

---
+++ quiz/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+class Quiz(models.Model):
+	title = models.CharField(null=True)
+	score = models.IntegerField(null=True)
+
+	def __str__(self):
+		return self.title	
+
+
+class Answer(models.Model):
+	text = models.CharField(max_length=100)	
+
+	def __str__(self):
+		return self.text
+
+class Question(models.Model):
+	quiz = models.ForeignKey(Quiz)
+	text = models.CharField(max_length=100)
+	answer = models.ForeignKey(Answer)
+
+	def __str__(self):
+		return self.text
+
+
+
